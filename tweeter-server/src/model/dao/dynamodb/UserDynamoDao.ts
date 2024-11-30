e98248464430c47a58f822d5111ce8bf776a1927
Logan Brewer <lbrewer4@byu.edu>
2024-11-30 09:22:39 -0700

    feat: Write batchGetUsersByAlias method

--- tweeter-server/src/model/dao/dynamodb/UserDynamoDao.ts
+++ tweeter-server/src/model/dao/dynamodb/UserDynamoDao.ts
@@ -2,21 +2,19 @@ import {
   DynamoDBDocumentClient,
   GetCommand,
   PutCommand,
-  QueryCommand,
-  DeleteCommand,
+  BatchGetCommand,
 } from "@aws-sdk/lib-dynamodb";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { User, UserDto } from "tweeter-shared";
+import { UserDAO } from "../interfaces/UserDAO";
 
-export class UserDynamoDAO {
+export class UserDynamoDAO implements UserDAO {
   readonly tableName = "User";
   readonly pkAttr = "alias";
 
   private readonly client = DynamoDBDocumentClient.from(new DynamoDBClient());
 
-  async getUser(
-    alias: string
-  ): Promise< UserDto | null> {
+  async getUser(alias: string): Promise<User | null> {
     const params = {
       TableName: this.tableName,
       Key: {
@@ -24,32 +22,68 @@ export class UserDynamoDAO {
       },
     };
 
-    const output: UserDto = await this.client.send(new GetCommand(params));
-    return output.Item || null;
+    const output = await this.client.send(new GetCommand(params));
+
+    if (!output.Item) {
+      return null;
+    }
+
+    const dto: UserDto = {
+      alias: output.Item.alias,
+      imageUrl: output.Item.imageUrl,
+      firstName: output.Item.firstname,
+      lastName: output.Item.lastname,
+    };
+
+    return User.fromDto(dto);
   }
 
-  async createUser(alias: string, image: string): Promise<void> {
+  async createUser(newUser: User): Promise<void> {
     const params = {
       TableName: this.tableName,
       Item: {
-        [this.pkAttr]: alias,
-        image,
+        [this.pkAttr]: newUser.alias,
+        imageUrl: newUser.imageUrl,
+        firstname: newUser.firstName,
+        lastname: newUser.lastName,
       },
     };
     await this.client.send(new PutCommand(params));
   }
 
-  async updateUserImage(alias: string, image: string): Promise<void> {
+  async batchGetUsersByAliases(aliases: Set<string>): Promise<User[]> {
+    if (aliases && aliases.size === 0) {
+      return [];
+    }
+
+    const aliasArray = Array.from(aliases);
+
+    const keys = aliasArray.map<Record<string, {}>>((alias) => ({
+      [this.pkAttr]: alias,
+    }));
+
     const params = {
-      TableName: this.tableName,
-      Key: {
-        [this.pkAttr]: alias,
-      },
-      UpdateExpression: "SET image = :img",
-      ExpressionAttributeValues: {
-        ":img": image,
+      RequestItems: {
+        [this.tableName]: {
+          Keys: keys,
+        },
       },
     };
-    await this.client.send(new UpdateCommand(params));
+
+    const result = await this.client.send(new BatchGetCommand(params));
+
+    if (result.Responses) {
+      return result.Responses[this.tableName].map<User>(
+        (item) =>
+          User.fromDto({
+            alias: item[this.pkAttr],
+            imageUrl: item.imageUrl,
+            firstName: item.firstName,
+            lastName: item.lastName,
+          })!
+      );
+    } else {
+      return [];
+    }
   }
 }
--- tweeter-server/src/model/dao/interfaces/UserDAO.ts
+++ tweeter-server/src/model/dao/interfaces/UserDAO.ts
@@ -2,7 +2,6 @@ import { User } from "tweeter-shared";
 
 export interface UserDAO {
   createUser(user: User): Promise<void>;
-  getUserByAlias(alias: string): Promise<User | null>;
-  batchGetUsersByAliases(aliases: string[]): Promise<User[]>;
-  updateUserImage(alias: string, imageUrl: string): Promise<void>;
+  getUser(alias: string): Promise<User | null>;
+  batchGetUsersByAliases(aliases: Set<string>): Promise<User[]>;
 }
