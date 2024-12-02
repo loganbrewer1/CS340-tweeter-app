52af04edef8e71c1834a2628b350e8098ba407f1
Logan Brewer <lbrewer4@byu.edu>
2024-12-02 11:50:02 -0700

    feat: error handling for login

--- tweeter-server/src/lambda/user/LoginLambda.ts
+++ tweeter-server/src/lambda/user/LoginLambda.ts
@@ -1,19 +1,36 @@
-import { LoginRequest, LoginResponse } from "tweeter-shared";
+import { AuthToken, LoginRequest, LoginResponse, User } from "tweeter-shared";
 import { UserService } from "../../model/service/UserService";
 
 export const handler = async (
   request: LoginRequest
 ): Promise<LoginResponse> => {
   const userService = new UserService();
-  const [user, authToken] = await userService.login(
-    request.alias,
-    request.password
-  );
+  try {
+    const [user, authToken] = await userService.login(
+      request.alias,
+      request.password
+    );
+    return {
+      success: true,
+      message: null,
+      token: authToken.token,
+      user: user.dto,
+    };
+  } catch (e) {
+    if ((e as Error).message === "Invalid alias or password.") {
+      return {
+        success: false,
+        message: "Invalid alias or password.",
+        token: "",
+        user: new User('','','','').dto,
+      };
+    }
+  }
 
-  return {
-    success: true,
-    message: null,
-    token: authToken.token,
-    user: user.dto,
-  };
+    return {
+      success: false,
+      message: "An unknown error occurred.",
+      token: "",
+      user: new User("", "", "", "").dto,
+    };
 };
